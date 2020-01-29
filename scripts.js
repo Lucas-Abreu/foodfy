c09418d6f7e1f673a4b912780c08379e81664b78
Lucas <abreu.lucass@outlook.com>
2020-01-29 15:46:16 -0300

    finished
    working as intended.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
 const cards = document.querySelectorAll('.card')
+const modalOverlay = document.querySelector('.modal-overlay')
 
 for (let card of cards)
 {
     card.addEventListener('click', function()
     {
-        document.querySelector('.modal-overlay').classList.add('active');
+        document.querySelector('.modal-image img').setAttribute('src', card.querySelector('img').src)
+        
+        document.querySelector('.modal-title').innerHTML = card.querySelector('.card-title').innerHTML
+
+        document.querySelector('.modal-other').innerHTML = card.querySelector('.card-author').innerHTML
+
+        modalOverlay.classList.add('active');
     })
 }
+
+document.querySelector('.close-modal').addEventListener('click', function()
+{
+    modalOverlay.classList.remove('active')
+})
